7a3e76a2075363f8f10f3e498c99bebd67ce1c24
GopalDas123 <Gopaldax@gmail.com>
2020-06-13 15:19:04 +0500

    Add to gitHub

---
+++ src/Room.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Room() {
+  return (
+    <div>
+      This is room component
+    </div>
+  );
+}
+
+export default Room;
